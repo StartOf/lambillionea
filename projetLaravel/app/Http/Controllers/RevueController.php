9f0c8007da99b4bf3d5b2d61716f1d517a76c10a
StartOf <mathieulangue.louis@gmail.com>
2016-04-28 10:55:14 +0200

    :)

---
+++ projetLaravel/app/Http/Controllers/RevueController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//on charge le modèle dont le controlleur a besoin
+use App\Revue;
+
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class RevueController extends Controller
+{
+    //
+}
